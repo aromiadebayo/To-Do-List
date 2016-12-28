0a4c83d3955e490f77661858899acd972a9c70ee
aromiadebayo <adebayo.andrew97@gmail.com>
2016-12-27 22:40:36 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,69 +1,40 @@
-const getInput  = document.querySelector('input');
-const getAddButton = document.querySelector('#addItem');
-const getList = document.querySelector('ul');
-const getDeleteButton = document.querySelector('#removeItem');
+const addItemButton = document.querySelector('button');
+const addInputList = document.querySelector('input');
+const body = document.querySelector('body');
 
+addItemButton.addEventListener('click', addWell);
+/*********************
+BUTTON CLICK EVENTS
+********************/
 
-/*********************************************
-Button Click Events
-********************************************/
-function addstuff(){
-  if(getInput.value == ''){
-    alert("Add an Item");
+function addWell(){
+  if(addInputList.value == ""){
+    alert("Insert Item");
   }else{
-    let li = document.createElement('li');
-    getList.appendChild(li);
-    let button = document.createElement('button');
-    getList.appendChild(button);
-    button.textContent = "Completed";
-    button.className = "finishButton";
-    let getListItem = document.querySelector('li');
-    li.textContent = getInput.value;
-    getInput.value = '';
+  const newWell = document.createElement('div');
+  body.appendChild(newWell);
+  newWell.className = "well well-lg";
+  newWell.textContent = addInputList.value;
+  const completeButton = document.createElement('button');
+  newWell.appendChild(completeButton);
+  addInputList.value = "";
+  completeButton.textContent = "Completed";
+  completeButton.className = "btn btn-default removeItem pull-right";
+  completeButton.addEventListener('click', deleteItem);
+  function deleteItem(){
+    body.removeChild(newWell);
   }
 }
-
-//create list item when button is pressed
-getAddButton.addEventListener('click',addstuff);
-//delete list item when button is pressed
-getDeleteButton.addEventListener('click', () => {
-let li = document.querySelector('li');
-let but = document.querySelector('button');
-getList.removeChild(li) ;
-getList.removeChild(but);
-});
-
-/**************************************
-Mouse Hover Events
-***************************************/
-/////////////////////
-//Buttons//
-//////////////////////
-function hoverBlueButton(){
-  getAddButton.style.opacity = 0.7;
-
 }
-function hoverRedButton(){
-  getDeleteButton.style.opacity = 0.7;
 
-}
-function leaveBlueButton(){
-  getAddButton.style.opacity = 1;
 
-}
-function leaveRedButton(){
-  getDeleteButton.style.opacity = 1;
-
-}
-getAddButton.addEventListener('mouseover',hoverBlueButton);
-getDeleteButton.addEventListener('mouseover', hoverRedButton);
-getAddButton.addEventListener('mouseleave',leaveBlueButton);
-getDeleteButton.addEventListener('mouseleave', leaveRedButton);
-
-//////////////////////
-//List Items//
-////////////////
 
 /*************************
-KeyBoard Events
-************************/
+KEYBOARD PRESS EVENTS
+*********************/
+addInputList.addEventListener('keydown', (event) => {
+  if(event.keyCode == 13 || event.which == 13){
+    addWell();
+    event.preventDefault();
+  }
+});
